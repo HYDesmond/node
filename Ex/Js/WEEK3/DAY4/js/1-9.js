c74942c24692f242241e98a1935360b0391f2de2
HYDesmond <jlevor@163.com>
2018-07-02 10:44:27 +0800

    upload

---
+++ Ex/Js/WEEK3/DAY4/js/1-9.js
@@ -0,0 +1,9 @@
+utils.css(box, {
+    width: 300,
+    height: 200,
+    padding: 30,
+    opacity: 1,
+    background: 'orange'
+});
+utils.css(box, 'border', '5px dashed red');
+console.log(utils.css(box, 'margin'));
